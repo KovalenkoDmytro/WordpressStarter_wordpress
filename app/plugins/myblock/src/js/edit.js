1eb77a1fef61efcf264803e255cc6db7a509b228
Dmytro Kovalenko <dmytro@webeeline.com>
2024-07-06 20:07:31 -0600

    feat: added ESLint for JS

--- app/plugins/myblock/src/js/edit.js
+++ app/plugins/myblock/src/js/edit.js
@@ -1,7 +1,7 @@
 import { useBlockProps } from '@wordpress/block-editor';
-import "../style/editor.scss";
+import '../style/editor.scss';
 
 export default function Edit() {
-    const blockProps = useBlockProps();
-    return <h1 {...blockProps}>Edit 6</h1>;
+	const blockProps = useBlockProps();
+	return <h1 {...blockProps}>Edit 6</h1>;
 }
